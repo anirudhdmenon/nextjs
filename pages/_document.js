3ffb123a48ac31860cbad0e0f79cb7bdfede3eee
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-08 17:57:17 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-      <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js"></script> 
-      </Head>
+      <Head/>
       <body>
         <Main />
         <NextScript />
