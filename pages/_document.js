b1a5acd1ff8f8b85dc60f3019b0e4525117314ce
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-14 17:10:18 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js" async></script>
+        <script async  data-cookieyes="cookieyes-analytics" src="index"></script>
       </Head>
       <body>
         <Main />
-        <iframe
-                  width="800"
-                  height="400"
-                  src="https://www.youtube.com/embed/a3ICNMQW7Ok"
-                  title="YouTube video player"
-                  allowfullscreen
-                ></iframe>
         <NextScript />
       </body>
     </Html>
