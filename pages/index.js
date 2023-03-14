d5299bccf350865a6aba80b0197ec2fa6f5721aa
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-14 16:34:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,42 @@
 export default function Home() {
   return (
-    <Html lang="en">
-      <Head>
-        <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js" async></script>
-      </Head>
-        <body>
-          <div>
-            <h1 className="text-2xl text-center font-bold">ADM Car Rentals India Pvt. Ltd.</h1>
-            <h3 className="text-2lg text-center my-6 font-bold">From the No.1 Best Car Dealers in India</h3>
-            <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
-              quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
-              eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
-              Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
-              Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
-              neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
-              vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
-              dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
-            
-            <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
-              quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
-              eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
-              Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
-              Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
-              neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
-              vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
-              dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
-            <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
-              quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
-              eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
-              Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
-              Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
-              neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
-              vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
-              dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
-              <div className="flex justify-center mb-5">
-                <iframe
-                  width="800"
-                  height="400"
-                  src="https://www.youtube.com/embed/a3ICNMQW7Ok"
-                  title="YouTube video player"
-                  allowfullscreen
-                ></iframe>
-              </div>
-          </div>
-        </body>
-    </Html>
+    <div>
+      <h1 className="text-2xl text-center font-bold">ADM Car Rentals India Pvt. Ltd.</h1>
+      <h3 className="text-2lg text-center my-6 font-bold">From the No.1 Best Car Dealers in India</h3>
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
+        quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
+        eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
+        Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
+        Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
+        neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
+        vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
+        dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
+      
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
+        quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
+        eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
+        Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
+        Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
+        neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
+        vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
+        dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
+      <p className="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec vehicula augue. In blandit justo ex, 
+        quis viverra nisi commodo ut. Morbi molestie finibus iaculis. Praesent luctus, quam id placerat mollis, 
+        eros ex tempus dui, eget ultricies erat nisi in ipsum. Quisque eu auctor libero, sed imperdiet ante. 
+        Nullam pellentesque sodales venenatis. Mauris semper ex orci, sed sollicitudin purus sodales vitae. 
+        Pellentesque consectetur dolor vel mauris posuere efficitur. Duis in orci enim. Vivamus et efficitur 
+        neque. Fusce malesuada tellus sed enim rhoncus, id vestibulum justo commodo. Quisque id velit non ex 
+        vehicula facilisis non at orci. Nunc cursus eleifend fringilla. Mauris posuere, dui in dictum rhoncus, 
+        dolor augue posuere erat, quis sagittis urna mi eu est. Praesent non purus ac elit scelerisque sodales.</p>
+        <div className="flex justify-center mb-5">
+          <iframe
+            width="800"
+            height="400"
+            src="https://www.youtube.com/embed/a3ICNMQW7Ok"
+            title="YouTube video player"
+            allowfullscreen
+          ></iframe>
+        </div>
+    </div>
   )
 }
