402a3b720ef6b173c92dcf4c4a7c4c9e4400a196
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-16 10:00:32 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js" async></script>
-        <script async data-cookieyes="cookieyes-analytics" src="index"></script>
+      <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js"></script>
       </Head>
       <body>
         <Main />
