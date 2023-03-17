a65cb3ea6f460b7adcb71fe05fce38d91d84417e
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-17 12:06:03 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <Script 
+          src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js"
+          strategy="beforeInteractive">
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
