7ab5035ad9b28337a816ad08c2c2cac99a6c0c10
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2023-03-17 11:28:53 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import "tailwindcss/tailwind.css";
 import Layout from "@/components/Layout";
+import Script from 'next/script';
+import { Html } from "next/document";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <div className="w-5/6 m-auto">
-        <Component {...pageProps} />
-      </div>
-    </Layout>
+    <Html lang="en">
+      <head>
+        <script
+          src="https://scriptstaging.cookieyes.com/client_data/43d91c49ab844efb74ac5f76/script.js"
+          strategy="beforeInteractive">
+        </script>
+      </head>
+      <Layout>
+        <div className="w-5/6 m-auto">
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </Html>
   )
 }
