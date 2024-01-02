20194232ea40c15c242e3c74400a85d6351926a6
anirudhdmenon <48902358+anirudhdmenon@users.noreply.github.com>
2024-01-02 12:49:28 +0530

    Update _app.js
    nn

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,10 @@ import Script from 'next/script';
 export default function RootLayout({ Component, pageProps }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <script id="cookieyes" type="text/javascript" src="https://scriptstaging.cookieyes.com/client_data/6064be5b3d1d7ba2c47fd42b/script.js"></script>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9884004269444664" crossorigin="anonymous"></script>
-      </head>
+      </Head>
       <Layout>
         <div className="w-5/6 m-auto">
           <Component {...pageProps} />
